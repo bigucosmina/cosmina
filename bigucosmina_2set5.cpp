8276d70acfcde53336100ba18d0e86264b0144f9
bigucosmina <45461038+bigucosmina@users.noreply.github.com>
2018-11-29 16:51:57 +0200

    Add files via upload

---
+++ bigucosmina_2set5.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int n;
+int main()
+{
+    cout<<"n=";cin>>n;
+    int x=0;
+    while (n>0)
+    {
+        n=n>>1;
+        x=x+1;
+    }
+    while(n=0)
+}
+    n=n;
+    x=x;
+    }
+    while(n<0)
+    {
+        n=n<<0;
+        x=x-1;
+    }
+    cout<<x;
+    return 0;
+}
